8c604119d8f68793b2feb4e338d3f03c6623d2b4
Tore Aurstad <tore.aurstad@gmail.com>
2026-01-11 01:30:08 +0100

    Added editform demo

--- Components/Demos/BuiltInComponents/Sampledata/Superhero.cs
+++ Components/Demos/BuiltInComponents/Sampledata/Superhero.cs
@@ -1,11 +1,18 @@
-﻿namespace DependencyInjectionDemo.Components.Demos.BuiltInComponents.Sampledata
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DependencyInjectionDemo.Components.Demos.BuiltInComponents.Sampledata
 {
    
     public class Superhero
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
         public string SpecialAbility { get; set; } = string.Empty;
+
     }
 
 }
