047b29ac0edcec624ac71bba646abef9ad7328c2
Tore Aurstad <tore.aurstad@gmail.com>
2026-01-24 22:42:06 +0100

    Added templated component Alert.razor

---
+++ Components/Demos/TemplatedComponents/AlertTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace DependencyInjectionDemo.Components.Demos.TemplatedComponents
+{
+    public enum AlertTypeEnum
+    {
+        Primary,
+        Secondary,
+        Success,
+        Danger, 
+        Warning, 
+        Info,
+        Light,
+        Dark
+    }
+}
