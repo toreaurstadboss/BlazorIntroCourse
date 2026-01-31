e3bb692d0efba36f21e390a32349830de72d76d2
Tore Aurstad <tore.aurstad@gmail.com>
2026-02-01 00:18:16 +0100

    Looked more on DotNetObjectReference and DotNet and static methods and JsInvokable

--- Components/Demos/Javascript/Components/JsToReferenceNet.razor.js
+++ Components/Demos/Javascript/Components/JsToReferenceNet.razor.js
@@ -1,3 +1,12 @@
 ﻿export function callreferencenetfromjs (dotNetHelper) {
     return dotNetHelper.invokeMethodAsync('GetHelloMessage', 'Blazor');
 };
+
+
+window.callnetfromjs = () => {
+    
+    DotNet.invokeMethodAsync('DependencyInjectionDemo', 'NameOfTheMethod').
+        then(data => {
+            document.getElementById("StaticResultId").innerText = data;
+        });
+}
